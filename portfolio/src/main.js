c0d6bff725fc5c19d926196df55832c9bb8c2a65
Josefine <Josefine.magnussen@hotmail.com>
2019-07-15 23:12:44 +0200

    fullpage.js added

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
+import VueFullPage from 'vue-fullpage.js'
+
+Vue.use(VueFullPage);
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
+    router,
+    store,
+    render: function(h) { return h(App) }
 }).$mount('#app')
--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <full-page ref="fullpage" :options="options" id="fullpage">
+    <div class="section">
+<HelloWorld/>
+    </div>
+    <div class="section">
+      <About/>
+    </div>
+     <div class="section">
+      3 section ...
+    </div>
+  </full-page>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import About from './About'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
+  HelloWorld,
+   About
+  },
+   data() {
+    return {
+      options: {
+        menu: '#menu',
+        licensKey: 'YOUR_KEY_HERE',
+        anchors: ['Home', 'HelloWorld', 'About'],
+        sectionsColor: ['#faa536', '#f47828', '#bdce30']
+      },
+}},
 }
 </script>
