2d415a1bea96a45be0190874dcf5dcf9edf9c0d2
Josefine Magnussen <josefine@getacademy.no>
2019-08-07 14:27:13 +0200

    new index page

---
+++ Activiti Form/adsate - css2/src/components/Index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div >
+ 
+<div class="info">
+  <h2>Info</h2>
+</div>
+ <div class="img"> 
+
+<img src="../img/undraw_activity_tracker_1l9o.svg" alt="activity">
+</div>
+
+    <footer>
+
+    </footer>
+  </div>
+</template>
+
+
+
+import Axios from "axios";
+
+export default {
+  name: "LoadData",
+  components: {
+    svgArt,
+  },
+  props: ["personprop"],
+  data() {
+    return {
+      person: {
+
+      }
+    };
+  },
+};
+</script>
+<style >
+@import "../style/menu.css";
+</style>
+<style scoped >
+
+</style>
+
