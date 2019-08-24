0ccdd0a0c4e0563adad527a787d893215dc49221
Josefine Magnussen <josefine@getacademy.no>
2019-08-24 21:48:21 +0200

    ShoppingList
    New prosject

---
+++ ShoppingList/src/components/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="shopping-list">
+  <h1>Hi Vue!</h1>
+  <h2>{{ header}}</h2>
+  <input type="text" v-model="header">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      header: 'Vue is ready to Pary!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
