8fa179ff5b91bc9d7b4d98d6d93d8bcb0bf6455e
Josefine Magnussen <josefine@getacademy.no>
2019-11-07 18:41:50 +0100

    ++

---
+++ Todo-App/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+<todo-list v-bind:todos="todos"></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+
+export default {
+  components: {
+    TodoList,
+  },
+
+  data(){
+    return{
+      todos:[{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      },
+      {
+        title: 'Todo B',
+        project: 'Project B',
+        done: false,
+      },
+      {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      },
+      {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }
+      
+      
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Todo-App/src/components/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="ui centered card">
+        <div class="content">
+            <div class="header">
+                {{todo.title}}
+            </div>
+            <div class="meat">
+                {{todo.project}}
+            </div>
+            <div class="extra content">
+                <span class="right floated edit icon">
+                    <i class="edit icon"></i>
+                </span>
+            </div>
+        </div>
+        <div class="ui bottom attached green basic button" v-show="todo.done">
+            Completed
+        </div>
+        <div class="ui bottom attached red basic button">
+            Complete
+        </div>
+
+        <my-component v-for="(item, index) in items" v-bind:item="item"  v-bind:index="index">
+</my-component>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['todo'],
+}
+</script>
